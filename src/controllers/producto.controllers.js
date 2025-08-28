6bea3fd48a1c1e4fca64a0e81d6a76b97d47fa50
JoseBaza <musulmanco@gmail.com>
2025-08-28 17:42:22 -0300

    se termina la ruta y el controlador de productos

--- src/controllers/producto.controllers.js
+++ src/controllers/producto.controllers.js
@@ -4,3 +4,13 @@ export const prueba = (req, res) => {
     res.status(201);
     res.send('Este es un mensaje desde el controlador')
  }
+
+ export const crearProducto = async (req, res) => {
+  try {
+    //console.log(req),
+    console.log(req.body)
+    res.send('desde la logica de crear producto!')
+  } catch (error) {
+    console.error(error)
+  }
+}
