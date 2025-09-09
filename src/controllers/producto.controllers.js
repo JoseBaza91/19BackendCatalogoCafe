89386759128a37cebcfe0b74e3760c47420f95fd
JoseBaza <musulmanco@gmail.com>
2025-09-09 13:59:47 -0300

    se crea el controlador de obtener productos

--- src/controllers/producto.controllers.js
+++ src/controllers/producto.controllers.js
@@ -22,3 +22,19 @@ export const crearProducto = async (req, res) => {
       .json({ mensaje: "Error al crear el producto", error: error.message });
   }
 };
+
+export const obtenerProductos = async (req, res)=>{
+    try {
+        //1 - Buscar los productos en la base de datos
+        const listaProductos = await Producto.find();
+        //2 - responder con el status adecuado 200 y devolver los productos
+        res.status(200).json(listaProductos);
+    } catch (error) {
+        console.error(error)
+        res.status(500).json({ mensaje: "Error al obtener los productos" });
+    }
+}
+
+export const obtenerProducto = async (req, res)=>{
+    
+}
