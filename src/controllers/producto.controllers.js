b27fda801df5f4486b76d64bf389bde3dbfcb46f
JoseBaza <musulmanco@gmail.com>
2025-09-15 18:48:01 -0300

    agregamos logica para borrar productos por Id

--- src/controllers/producto.controllers.js
+++ src/controllers/producto.controllers.js
@@ -39,7 +39,6 @@ export const obtenerProductoPorId = async (req, res) => {
   try {
     //1- buscar el producto por el campo del id
     const productoBuscado = await Producto.findById(req.params.id);
-    console.log(productoBuscado);
     //2- chequear que encontre el producto, sino existe enviar mensaje de error
     if (!productoBuscado) {
       return res.status(404).json({ mensaje: "Producto no encontrado" });
@@ -49,5 +48,21 @@ export const obtenerProductoPorId = async (req, res) => {
   } catch (error) {
     console.error(error);
     res.status(500).json({ mensaje: "Error al obtener el producto por ID" });
+  }
+};
+
+export const borrarProductoPorId = async (req, res) => {
+  try {
+    //1-pedir a mongoose eliminar un producto por el id
+    const productoEliminado = await Producto.findByIdAndDelete(req.params.id);
+    //2- verificar que pude encontrar el producto a eliminar
+    if (!productoEliminado) {
+      return res.status(404).json({ mensaje: "Producto no encontrado" });
+    }
+    //3-enviar la respuesta que pude eliminar el producto
+    res.status(200).json({ mensaje: "Producto eliminado correctamente" });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ mensaje: "No se pudo eliminar el producto por ID" });
   }
 };
